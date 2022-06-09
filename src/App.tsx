6f1e462333826500bb352680e155c64400cf49a3
wgshtg <championshipnwang@gmail.com>
2022-06-09 14:21:54 +0800

    Remove useless header

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import './App.css';
 import React from 'react';
 import { Link, Route, Routes } from 'react-router-dom';
 
-import logo from './logo.svg';
 
 // import tickingClock from './tickingClock';
 
@@ -13,20 +12,6 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
       <h1>Welcome to React Router!</h1>
       <Routes>
         <Route path="/" element={<Home />} />
